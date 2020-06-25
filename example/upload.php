1dd4024f20db08f4522ce4b935df782f92c482e6
brunoribeiro-lab <bruno.espertinho@gmail.com>
2020-06-25 17:48:29 -0300

    v1.0

---
+++ example/upload.php
@@ -0,0 +1,14 @@
+<?php
+
+include("../autoload.php");
+
+$upload = new Upload('img');
+$upload
+        ->file_name('uploaded')
+        ->upload_to('upload/')
+        ->run();
+if (!$upload->was_uploaded) {
+    die('error : ' . $upload->error);
+} else {
+    echo 'image sent successfully !';
+}
